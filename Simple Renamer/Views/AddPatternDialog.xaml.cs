588f144d7019a21238ad17b877ad03f27776b029
Mauricio Cerón Medina <sistemas@businessparts.com>
2023-08-15 12:52:12 -0600

    Change namespace

--- Simple Renamer/Views/AddPatternDialog.xaml.cs
+++ Simple Renamer/Views/AddPatternDialog.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Simple_Renamer.Views
+namespace Simple_Renamer
 {
     /// <summary>
     /// Interaction logic for AddPatternDialog.xaml
